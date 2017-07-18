c6ded1fe5081c511095a4be9781e616bd2347d98
Dima Kovalchuk <dmyutro@ukr.net>
2017-07-18 14:32:51 +0300

    Add signals for book reading

---
+++ book/signals.py
@@ -0,0 +1,24 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from book.models import Book
+from book.models import BookReading
+from currency.models import Opportunity
+
+
+@receiver(post_save, sender=Book)
+def opportunities_on_book_creating(sender, instance, created, **kwargs):
+    if created:
+        Opportunity.objects.create(
+            user=instance.owner,
+            type=Opportunity.ADD_BOOK
+        )
+
+
+@receiver(post_save, sender=BookReading)
+def opportunities_on_book_reading(sender, instance, created, **kwargs):
+    if created:
+        Opportunity.objects.create(
+            user=instance.user,
+            type=Opportunity.READ_BOOK,
+        )
