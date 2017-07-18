754d58f106df2be1932b644b0446635df364ab3c
Dima Kovalchuk <dmyutro@ukr.net>
2017-07-18 18:38:42 +0300

    User about and avatar

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Additional info', {'fields': ('about', 'avatar', )}),
+    )
+
 
 admin.site.register(User, UserAdmin)
--- users/models.py
+++ users/models.py
@@ -1,9 +1,21 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
 from currency.models import Opportunity
+from ckeditor.fields import RichTextField
 
 
 class User(AbstractUser):
+
+    avatar = models.ImageField(
+        upload_to='avatars',
+        verbose_name=_('Avatar'),
+        null=True,
+    )
+    about = RichTextField(
+        verbose_name=_('About'),
+    )
 
     @property
     def opportunities(self):
