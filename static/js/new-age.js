7bab56522f46225b1226ea3865628e2d746399db
Dima Kovalchuk <dmyutro@ukr.net>
2017-07-26 19:14:14 +0300

    Add missing js

---
+++ static/js/new-age.js
@@ -0,0 +1,52 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $(document).on('click', 'a.page-scroll', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 100
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function() {
+        $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 50
+        }
+    })
+
+    // Index statistics counter 
+    var finished = false;
+    $(window).scroll(function() {
+        var oTop = $('#statistics').offset().top - window.innerHeight;
+        if (finished == false && $(window).scrollTop() > oTop) {
+            $('#statistics .count').each(function () {
+                $(this).prop('Counter',0).animate({
+                    Counter: $(this).text()
+                }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function (now) {
+                        $(this).text(Math.ceil(now));
+                    }
+                });
+            });
+            finished = true;
+        }
+     });
+
+
+})(jQuery); // End of use strict
