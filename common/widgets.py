4dfff6499339699d901f012821b0da40b3558170
Dima Kovalchuk <dmyutro@ukr.net>
2017-08-01 12:32:59 +0300

    Fix gulp copy static

---
+++ common/widgets.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ClassName(object):
+    """docstring for ClassName"""
+    def __init__(self, arg):
+        super(ClassName, self).__init__()
+        self.arg = arg
--- gulpfile.js
+++ gulpfile.js
@@ -53,14 +53,19 @@ gulp.task('minify-js', function() {
 // Copy lib libraries from /node_modules into /lib
 gulp.task('copy', function() {
     gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
-        .pipe(gulp.dest('lib/bootstrap'))
+        .pipe(gulp.dest('static/lib/bootstrap'))
 
     gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('lib/jquery'))
+        .pipe(gulp.dest('static/lib/jquery'))
 
     gulp.src(['node_modules/simple-line-icons/*/*'])
-        .pipe(gulp.dest('lib/simple-line-icons'))
+        .pipe(gulp.dest('static/lib/simple-line-icons'))
 
+    gulp.src([
+            'node_modules/croppie/*.css',
+            'node_modules/croppie/*.min.js',
+        ])
+        .pipe(gulp.dest('static/lib/croppie'))
 
     gulp.src([
             'node_modules/font-awesome/**',
@@ -70,7 +75,7 @@ gulp.task('copy', function() {
             '!node_modules/font-awesome/*.md',
             '!node_modules/font-awesome/*.json'
         ])
-        .pipe(gulp.dest('lib/font-awesome'))
+        .pipe(gulp.dest('static/lib/font-awesome'))
 })
 
 // Run everything
