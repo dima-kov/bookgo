91e5cb3453f445334dadb729472c3e96d85784ce
Dima <dima.kovalchuk.v@gmail.com>
2018-03-28 04:46:42 +0300

    Drop opps from on register

--- users/signals.py
+++ users/signals.py
@@ -1,11 +1,11 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.db.models.signals import post_save
+# from django.dispatch import receiver
 
-from users.models import User
-from currency.models import Opportunity
+# from users.models import User
+# from currency.models import Opportunity
 
 
-@receiver(post_save, sender=User)
-def opportunities_on_register(sender, instance, created, **kwargs):
-    if created:
-        Opportunity.objects.create(user=instance, type=Opportunity.REGISTER)
+# @receiver(post_save, sender=User)
+# def opportunities_on_register(sender, instance, created, **kwargs):
+#     if created:
+#         Opportunity.objects.create(user=instance, type=Opportunity.REGISTER)
