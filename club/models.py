2ef6364d01ff9a1976c7978cf1d9f13dcf17e841
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2018-12-25 22:05:40 +0200

    Add base Club model

--- club/models.py
+++ club/models.py
@@ -1,10 +1,10 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 from users.models import User
 
 
 class Club(models.Model):
-
     name = models.CharField(
         max_length=255,
         verbose_name=_('Назва'),
@@ -12,3 +12,12 @@ class Club(models.Model):
     slug = models.SlugField(
         verbose_name=_('Slug')
     )
+    manger = models.ForeignKey(
+        User,
+        verbose_name=_('Менеджер'),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name = _('Клуб')
+        verbose_name_plural = _('Клуби')
