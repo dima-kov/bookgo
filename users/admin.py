bfbe207a05bb05b89f55e17a8259b8e854f49271
Dima Kovalchuk <dima.kovalchuk.v@gmail.com>
2018-12-29 17:43:31 +0200

    Registration: Fb field

--- users/admin.py
+++ users/admin.py
@@ -11,7 +11,7 @@ class UserAdmin(BaseUserAdmin):
 
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'fb')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
